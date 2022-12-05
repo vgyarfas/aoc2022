22449a6fe01b0603d6d629dfd87e05b24b22304d
Viktor Gyárfás <viktor.gyarfas@lightware.com>
2022-12-05 15:54:29 +0100

    formatting

--- src/day05/index.ts
+++ src/day05/index.ts
@@ -1,16 +1,37 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string): {containers: Array<Array<string>>, orders: Array<{moveNrOfUnits: number, fromIndex: number, toIndex: number}>} => {
-  const rows = rawInput.split('\n');
-  const splitIndex = rows.map(row => row.trim()).findIndex(row => row === '');
-  const splitRows = rows.slice(0, splitIndex - 1).map(row => row.match(/.{1,4}/g).map(item => item.substring(1, 2)));
+const parseInput = (
+  rawInput: string,
+): {
+  containers: Array<Array<string>>;
+  orders: Array<{ moveNrOfUnits: number; fromIndex: number; toIndex: number }>;
+} => {
+  const rows = rawInput.split("\n");
+  const splitIndex = rows
+    .map((row) => row.trim())
+    .findIndex((row) => row === "");
+  const splitRows = rows
+    .slice(0, splitIndex - 1)
+    .map((row) => row.match(/.{1,4}/g).map((item) => item.substring(1, 2)));
 
   const containers = [];
-  for(let i = 0; i < splitRows[0].length; i++) {
-    containers.push(splitRows.map((row) => row[i]).reverse().filter(item => item !== ' '));
+  for (let i = 0; i < splitRows[0].length; i++) {
+    containers.push(
+      splitRows
+        .map((row) => row[i])
+        .reverse()
+        .filter((item) => item !== " "),
+    );
   }
 
-  const orders = rows.slice(splitIndex + 1).map(row => row.split(' ').filter(item => Number.isInteger(+item))).map(item => ({moveNrOfUnits: +item[0], fromIndex: +item[1] - 1, toIndex: +item[2] - 1}));
+  const orders = rows
+    .slice(splitIndex + 1)
+    .map((row) => row.split(" ").filter((item) => Number.isInteger(+item)))
+    .map((item) => ({
+      moveNrOfUnits: +item[0],
+      fromIndex: +item[1] - 1,
+      toIndex: +item[2] - 1,
+    }));
 
   return { containers, orders };
 };
@@ -18,27 +39,36 @@ const parseInput = (rawInput: string): {containers: Array<Array<string>>, orders
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  for(let order of input.orders) {
-    for(let i = 0; i < order.moveNrOfUnits; i++) {
-      if(input.containers[order.fromIndex].length > 0) {
-        input.containers[order.toIndex].push(input.containers[order.fromIndex].pop());
+  for (let order of input.orders) {
+    for (let i = 0; i < order.moveNrOfUnits; i++) {
+      if (input.containers[order.fromIndex].length > 0) {
+        input.containers[order.toIndex].push(
+          input.containers[order.fromIndex].pop(),
+        );
       }
     }
   }
 
-  return input.containers.map(container => container[container.length - 1]).join('');
+  return input.containers
+    .map((container) => container[container.length - 1])
+    .join("");
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  for(let order of input.orders) {
-    input.containers[order.toIndex].push(...input.containers[order.fromIndex].splice(-order.moveNrOfUnits, order.moveNrOfUnits));
+  for (let order of input.orders) {
+    input.containers[order.toIndex].push(
+      ...input.containers[order.fromIndex].splice(
+        -order.moveNrOfUnits,
+        order.moveNrOfUnits,
+      ),
+    );
   }
 
-  return input.containers.map(container => container[container.length - 1]).join('');
-
-  return;
+  return input.containers
+    .map((container) => container[container.length - 1])
+    .join("");
 };
 
 run({
